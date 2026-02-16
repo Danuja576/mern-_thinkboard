a8f74292b05382aa85b7574e43fcea9392db2be2
Danuja576 <danujatayanaka576j@gmail.com>
2026-02-15 23:29:39 -0700

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,36 @@
+import express from "express"
+import notesRoutes from "./Routes/notesRoutes.js"
+import {connectDB} from "./config/db.js"
+import dotenv from "dotenv"
+import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001
+
+
+//middleware
+app.use(express.json());// this middleware will pass the json bodies
+
+app.use(
+    cors({
+        origin:"http://localhost:5173",
+    })
+);
+app.use(rateLimiter);
+//our simple custom middleware
+// app.use((req,res,next) => {
+//    console.log(`Request method is ${req.method} & Req URL is ${req.url}`);
+//    next();
+// })
+
+app.use("/api/notes",notesRoutes);
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("Server Started on PORT:", PORT);
+    });
+});
+
